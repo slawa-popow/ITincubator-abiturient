447d88419252217f5feaf2002b7a4c8f6f356497
slawa-popow <110kwt@gmail.com>
2023-04-01 22:23:23 +0300

    freeCodeCamp 50%

--- typl/ts/fetchjson/index.js
+++ typl/ts/fetchjson/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = require("axios");
+// tsc -t es6 -m commonjs index.ts 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1.default.get(url).then((resp) => {
     console.log(resp.data);
--- typl/ts/fetchjson/index.ts
+++ typl/ts/fetchjson/index.ts
@@ -5,5 +5,5 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios.get(url).then((resp) => {
-    console.log(resp.data);
+    console.log(resp.data);  
 });
