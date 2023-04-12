372443313e23e251b6b9f37033a5c7b4a5f86f8e
slawa-popow <110kwt@gmail.com>
2023-04-12 23:16:21 +0300

    каркас

--- typl/examples_learn/examp.js
+++ typl/examples_learn/examp.js
@@ -2,93 +2,129 @@
 
 import { print } from "./mods/print.js";
 
-class PalindromDict {
-  #arrStr = [];
-  #dict = {};
 
-  constructor(arrStr) {
-    this.#arrStr = arrStr || [];
-    this.#fill(this.#arrStr);
-  }
 
-  #fill(arrStr) {
-    this.clear();
-    for (let v of arrStr) {
-      this.add(v, []);
-    }
-    return this;
-  }
+/**
+ * Добро пожаловать, авантюрист. Ваша цель - пройти лабиринт и добраться до финиша, не касаясь стен. Это убьет вас мгновенно!
+  Задача
+  Вам будет предоставлен двумерный массив лабиринта и массив направлений. Ваша задача - следовать данным указаниям. Если вы достигли конечной точки до того, как все ваши ходы закончились, вы должны вернуть Finish . Если вы наткнетесь на какие-либо стены или выйдете за границу лабиринта, вы должны вернуть Dead . Если вы все еще находитесь в лабиринте после использования всех ходов, вы должны вернуть Lost .
+  Массив лабиринта будет выглядеть так
 
-  clear(){
-    if (!this.isEmpty()) {
-      for (let k in this.#dict) {
-        delete this.#dict[k];
+  maze = [[1,1,1,1,1,1,1],
+          [1,0,0,0,0,0,3],
+          [1,0,1,0,1,0,1],
+          [0,0,1,0,0,0,1],
+          [1,0,1,0,1,0,1],
+          [1,0,0,0,0,0,1],
+          [1,2,1,0,1,0,1]]
+  ..со следующим ключом
+
+      0 = безопасное место для прогулок
+      1 = Стена
+      2 = Начальная точка
+      3 = Финишная точка
+  direction = ["N","N","N","N","N","E","E","E","E","E"] == "Finish"
+  Правила
+  1. Массив лабиринта всегда будет квадратным, т.е. N x N , но его размер и содержимое будут меняться от теста к тесту.
+  2. В финальных тестах места старта и финиша будут меняться.
+  3. Массив направлений всегда будет в верхнем регистре и будет иметь формат N = север, E = восток, W = запад и S = ​​юг .
+  4. Если вы достигли конечной точки до того, как все ваши ходы закончились, вы должны вернуть Finish .
+  5. Если вы наткнулись на любую стену или вышли за границу лабиринта, вы должны вернуть Dead .
+  6. Если вы все еще находитесь в лабиринте после использования всех ходов, вы должны вернуть Lost .
+ * 
+ */
+
+
+let mazerunner = (function(){
+
+  function AroundGo() {
+      if(this.constructor === AroundGo){
+          throw new Error('ERROR: Должен быть переопределен.');
       }
-    }
-    return this;
+  };
+  AroundGo.prototype.goTo = function(around) {
+      throw new Error("ERR: function of Abstract");
   }
 
-  isEmpty() {
-    return !Object.keys(this.#dict).length;
+// ------------------------------------------------------
+
+  function GoN() {
+      this.goTo = function(around) {
+          print('СЕВЕР ', around);
+      };
   }
 
-  add(key, value) {
-    if (!this.#dict.hasOwnProperty(key)) {
-      if (Array.isArray(value)) {
-        if (value.length) {
-          this.#dict[key].push(...value);
-        } 
+  function GoS() {
+      this.goTo = function(around) {
+          print('ЮГ ', around);
+      };
+  }
+
+  function GoW() {
+      this.goTo = function(around) {
+          print('ЗАПАД ', around);
+      };
+  }
+
+  function GoE() {
+      this.goTo = function(around) {
+          print('ВОСТОК ', around);
+      };
+  }
+
+// -------------------------------------------------------
+
+  GoN.prototype = Object.create(AroundGo.prototype);
+  GoS.prototype = Object.create(AroundGo.prototype);
+  GoW.prototype = Object.create(AroundGo.prototype);
+  GoE.prototype = Object.create(AroundGo.prototype);
+
+// --------------------------------------------------------
+
+  function MRunner() {
+      this._go = null;
+      this.strategy = {
+        goN: new GoN(),
+        goS: new GoS(),
+        goW: new GoW(),
+        goE: new GoE()
+      };
+      
+
+      Object.defineProperties(this, {
+        go: {
+          get: function() {
+              return this._go;
+          },
+          set: function(aroundObj) {
+              this._go = aroundObj;
+          }
+        }
+      });
+
+      this.goTo = function(around) {
+        if (this.go) {
+          this.go.goTo(around);
+        }
       }
-      this.#dict[key] = value;
-    } else {
-      this.#dict[key].push(value);
-    }
   }
 
-  
+  return {MRunner};
 
+})();
 
+function mazeRunner(maze, directions) {
+  let r = new mazerunner.MRunner();
+  r.go = r.strategy.goE;
+  r.goTo('E');
+  r.goTo('E');
+  r.go = r.strategy.goN;
+  r.goTo('N');
+  r.goTo('N');
+  r.goTo('N');
+
+  return r;
 }
 
-class LongPalindrome {
-  #_splitStr = [];
-  
-  constructor(str) {
-    this.splitStr = str;
-    this.dict = new PalindromDict(this.splitStr);
-  //   for (let i of [..."1234"]){
-  //     this.dict.add(i, i);
-  //   }
-  //   this.dict.add('baa', 888);
-  //   this.dict.add('baa', 'aAAAa');
 
-  }
-
-  get splitStr(){
-    return this.#_splitStr;
-  }
-
-  set splitStr(valueString) {
-    if (this.splitStr.length) {
-      this.#_splitStr.length = 0;
-    }
-    this.#_splitStr =  valueString.split(' ');
-  }
-
-  
-
-  
-}
-
-
-
-
-
-// in codewars Kata the NodeJS of 8.x version
- var longestPalindrome=function(s){
-    var lp = new LongPalindrome(s);
-
-    return lp.dict;
-  }
-
-  print(longestPalindrome('baa asdfg'));
+print(mazeRunner())
