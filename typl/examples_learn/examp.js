8b48f3c872088067040a2008651a19213084f5fd
slawa-popow <110kwt@gmail.com>
2023-04-19 15:56:27 +0300

    моржечок потiк

--- typl/examples_learn/examp.js
+++ typl/examples_learn/examp.js
@@ -35,8 +35,15 @@ import { print } from "./mods/print.js";
  */
 
 
-let mazerunner = (function(){
+/**
+ * @module
+ */
+const mazerunner = (function(){
 
+  /**
+   * @interface
+   * Интерфейс направления движения.
+   */
   function ArrowGo() {
       if(this.constructor === ArrowGo){
           throw new Error('ERROR: Должен быть переопределен.');
@@ -47,32 +54,42 @@ let mazerunner = (function(){
   }
 
 // ------------------------------------------------------
+/**
+ * Конструкторы реализующие интерфейс ArrowGo.
+ * Каждый из конструкторов реализует
+ * интерфейсный метод goTo(), который возвращает
+ * массив чисел (координаты i, j), которые нужно сложить с текущими
+ * координатами во время прохождения по лабиринту.
+ */
 
   function GoN() {
-      this.goTo = function(arrow) {
-          print('СЕВЕР ', arrow);
+      this.goTo = function() {
+          return [-1, 0];
       };
   }
 
   function GoS() {
-      this.goTo = function(arrow) {
-          print('ЮГ ', arrow);
+      this.goTo = function() {
+          return [1, 0];
       };
   }
 
   function GoW() {
-      this.goTo = function(arrow) {
-          print('ЗАПАД ', arrow);
+      this.goTo = function() {
+          return [0, -1];
       };
   }
 
   function GoE() {
-      this.goTo = function(arrow) {
-          print('ВОСТОК ', arrow);
+      this.goTo = function() {
+          return [0, 1];
       };
   }
 
 // -------------------------------------------------------
+  /**
+   * Наследоваться от абстрактного ArrowGo
+   */
 
   GoN.prototype = Object.create(ArrowGo.prototype);
   GoS.prototype = Object.create(ArrowGo.prototype);
@@ -81,27 +98,78 @@ let mazerunner = (function(){
 
 // --------------------------------------------------------
 
+  /**
+   * @constructor
+   * @param {Array} maze
+   * Лабиринт. 
+   */
   function Pole(maze) {
+    this._go = null;  // объект направления движения
+
     this.poleKeys = {
       0: "Lost",
       1: "Dead",
-      2: "Start",
+      2: "Lost",
       3: "Finish"
     };
 
+    // создать возможные направления для движения
+    this.strategy = {
+      N: new GoN(),
+      S: new GoS(),
+      W: new GoW(),
+      E: new GoE()
+    };
+
+    // геттер/сеттер направления движения
+    Object.defineProperties(this, {
+      go: {
+        get: function() {
+            return this._go;
+        },
+        set: function(arrow) {
+            this._go = this.strategy[arrow];
+        }
+      }
+    });
+
+    // копия двумерного массива, пришедшего извне
     this.maze = [...maze.map( (value) => {
       return [...value];
     })];
 
+    // найти числа 2 - начало и 3 - конец в массиве.
     [this.startArr, this.finishArr] = this.findStartFinish();
+    this.thisCoords = this.startArr;  // текущие координаты движения по массиву.
 
+    /**
+     * 
+     * Итератор. Создается с контекстом pole в MRunner().
+     * pole.go должен быть назначен до вызова next()
+     */
     this[Symbol.iterator] = function() {
       return {
-        start: this.startArr,
-        finish: this.finishArr,
         next() {
-          
+          let [i, j] = this.go.goTo.call(this); // Вызов с контекстом pole интерфесных методов направления движения.
+          this.thisCoords = [this.thisCoords[0] + i, this.thisCoords[1] + j]; // изменить координаты текущего положения
+
+          // Если текущие координаты не выходят за пределы массива, вернуть значение в массиве по координатам
+          if ((this.thisCoords[0] >= 0 && this.thisCoords[0] < this.maze.length) && (
+            (this.thisCoords[1] >= 0 && this.thisCoords[1] < this.maze.length)
+          )) {
+            let result = this.maze[this.thisCoords[0]][this.thisCoords[1]];
+            return {
+              done: false,
+              value: result
+          }
+          } else {
+            return {
+            done: true,
+            value: 1
+          }
+          }
         },
+          
         [Symbol.iterator](){return this},
       }
     };
@@ -109,6 +177,12 @@ let mazerunner = (function(){
   }
 
 // ----------------------------------------------------------
+  /**
+   * Ходим по двумерному массиву и ищем в нем числа 2 и 3.
+   * Если числа найдены, то завершаем проход по массиву,
+   * возвращаем координаты [i, j] чисел 2 - начала, 3 - конца.
+   * @returns { Array }
+   */
   Pole.prototype.findStartFinish = function() {
     const start = 2, finish = 3;
     let coordsStart = null, coordsFinish = null;
@@ -131,28 +205,21 @@ let mazerunner = (function(){
   } 
 // ----------------------------------------------------------
 
+  /**
+   * @constructor
+   * @param {Array} maze 
+   * Содержит массив направлений движения. (Внешняя сторона
+   * должна задать массив сразу после вызова конструктора).
+   * Итератор поля. (Вызов с контекстом pole).
+   */
   function MRunner(maze) {
-      this._go = null;
       this._directions = null;
-      this.pole = new Pole(maze);
+      this.pole = new Pole(maze); // лабиринт
+      // итератор по лабиринту
+      this.poleIterator = this.pole[Symbol.iterator].call(this.pole);
 
-      this.strategy = {
-        N: new GoN(),
-        S: new GoS(),
-        W: new GoW(),
-        E: new GoE()
-      };
-
+      // гет/сеттеры про массив направлений
       Object.defineProperties(this, {
-        go: {
-          get: function() {
-              return this._go;
-          },
-          set: function(arrowObj) {
-              this._go = arrowObj;
-              //this.pole.goTo = arrowObj;
-          }
-        },
         directions: {
           get: function() {
             return (this._directions != null) ? this._directions : [0];
@@ -163,32 +230,48 @@ let mazerunner = (function(){
         }
       });
 
+      /**
+       * 
+       * @param {string} arrow - получить из массива направлений символ 
+       * @returns {number} - число на текущем шаге в массиве
+       * Задать объект задающий алгоритм направления по лабиринту.
+       * Вызов next() с контекстом pole
+       */
+      this.nextStep = function(arrow) {
+        this.pole.go = arrow;
+        return this.poleIterator.next.call(this.pole).value;
+      };  
+  }
 
-      this.goTo = function() {
-        if (this.go) {
-          this.go.goTo.call(this.pole);
-        }
+  /**
+   * Итерация по массиву направлений.
+   * Передать символ направления для назначения алгоритма
+   * движения, сделать шаг по лабиринту (про итератор pole),
+   * вернуть, если найден, финиш, или мертв.
+   * После цикла результат движений по лабиринту.
+   * @returns {string}
+   */
+  MRunner.prototype.run = function(){
+    let resultSteps = null;
+    if (this.directions) {
+      for (let direct of this.directions) {
+        resultSteps = this.nextStep(direct);
+        if ((resultSteps === 3) || (resultSteps == 1)) { break; } 
       }
+
+      return this.pole.poleKeys[resultSteps];
+    }
   }
 
   return {MRunner};
 
 })();
 
+
 function mazeRunner(maze, directions) {
   let r = new mazerunner.MRunner(maze);
   r.directions = directions;
   
-  return r.pole;
+  return r.run();
 }
 
-
-print(mazeRunner([
-  [1,1,1,1,1,1,1],
-  [1,0,0,0,0,0,3],
-  [1,0,1,0,1,0,1],
-  [0,0,1,0,0,0,1],
-  [1,0,1,0,1,0,1],
-  [1,0,0,0,0,0,1],
-  [1,2,1,0,1,0,1]
-], ["N","N","N","N","N","E","E","E","E","E"]));
