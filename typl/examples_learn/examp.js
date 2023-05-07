b05229283916e4a4112d0b25cc1916801d152240
slawa-popow <110kwt@gmail.com>
2023-05-07 18:49:07 +0300

    ~~~~

--- typl/examples_learn/examp.js
+++ typl/examples_learn/examp.js
@@ -2,13 +2,3 @@
 // http://127.0.0.1:5500/typl/examples_learn/exam.html
 
 import { print } from "./mods/print.js";
-
-function squareSum(numbers){
-  return numbers.reduce( (pv, cv) => {
-    return pv + cv**2;
-  }, 0);
-}
-
-print(squareSum(
-  []
-));
