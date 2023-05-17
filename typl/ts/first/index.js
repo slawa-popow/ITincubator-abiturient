5abef6b96eeb97e5cac6fafe67f30500d203e2db
slawa-popow <110kwt@gmail.com>
2023-05-18 01:10:32 +0300

    006 learn

--- typl/ts/first/index.js
+++ typl/ts/first/index.js
@@ -3,7 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1.default.get(url).then(function (responce) {
-    console.log(responce.data);
+    var todo = responce.data;
+    var ID = todo.id;
+    var title = todo.title;
+    var finished = todo.completed;
+    console.log("ID: ".concat(ID, "\nTitle: ").concat(title, "\nFinished: ").concat(finished));
 }, function (err) {
     console.log('ERRor', err.code);
 });
--- typl/ts/first/index.ts
+++ typl/ts/first/index.ts
@@ -1,8 +1,19 @@
 import axios from 'axios'
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
+interface Todo {
+    id: number,
+    title: string,
+    completed: boolean
+}
 axios.get(url).then(responce => {
-    console.log(responce.data);
+    const todo = responce.data as Todo;
+    const ID = todo.id;
+    const title = todo.title;
+    const finished = todo.completed;
+
+    console.log(`ID: ${ID}\nTitle: ${title}\nFinished: ${finished}`);
+
 }, err => {
     console.log('ERRor', err.code);
 });
