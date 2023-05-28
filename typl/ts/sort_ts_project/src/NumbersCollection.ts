708e7cff46356590baab4bfcc9a16beb9d005418
slawa-popow <110kwt@gmail.com>
2023-05-28 23:35:28 +0300

    Рефакторинг класса сортировки введением нового класса хранящего массив и имеющего методы работы со своим массивом. Независимость, самостоятельность.

--- typl/ts/sort_ts_project/src/NumbersCollection.ts
+++ typl/ts/sort_ts_project/src/NumbersCollection.ts
@@ -2,14 +2,30 @@
 export class NumbersCollection {
     constructor(public data: number[]) {}
 
+    /**
+     * Длинна массива
+     */
     get length(): number {
         return this.data.length;
     }
 
+    /**
+     * Сравнить
+     * @param leftIndex 
+     * @param rightIndex 
+     * @returns 
+     */
     compare(leftIndex: number, rightIndex: number): boolean {
         return this.data[leftIndex] > this.data[rightIndex];
     }
 
+
+    /**
+     * Поменять местами значения
+     * this.data по переданным индексам (числам)
+     * @param leftIndex 
+     * @param rightIndex 
+     */
     swap(leftIndex: number, rightIndex: number): void {
         [this.data[leftIndex], this.data[rightIndex]] = [this.data[rightIndex], this.data[leftIndex]];
     }
--- typl/ts/sort_ts_project/src/Sorter.ts
+++ typl/ts/sort_ts_project/src/Sorter.ts
@@ -2,10 +2,13 @@
 import { NumbersCollection } from "./NumbersCollection";
 
 export class Sorter {
-    constructor(public collection: NumbersCollection) {}
+    constructor(public collection: NumbersCollection) {} 
 
+    /**
+     * Cортировка пузырьком
+     */
     sort(): void {
-        const { length } = this.collection;
+        const { length } = this.collection;  // тоже самое что и const length = this.collection.length
         for (let i = 0; i < length; i += 1) {
             for (let j = 0; j < length - i - 1; j += 1) {
                 if (this.collection.compare(j, j+1)) {
