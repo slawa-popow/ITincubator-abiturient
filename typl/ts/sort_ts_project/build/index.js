979553b7b2552e50cb1dcd51050eb1bd62f4e79c
slawa-popow <110kwt@gmail.com>
2023-05-28 23:30:35 +0300

    Рефакторинг класса сортировки введением нового класса хранящего массив и имеющего методы работы со своим массивом. Независимость, самостоятельность

--- typl/ts/sort_ts_project/build/index.js
+++ typl/ts/sort_ts_project/build/index.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('---7777777777777777777');
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numberCollection = new NumbersCollection_1.NumbersCollection([1, 5, 8, 0, -3, 12]);
+const sorter = new Sorter_1.Sorter(numberCollection);
+sorter.sort();
+console.log(sorter.collection.data);
--- typl/ts/sort_ts_project/src/index.ts
+++ typl/ts/sort_ts_project/src/index.ts
@@ -1,3 +1,8 @@
 
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-console.log('---OPPPP');
+const numberCollection = new NumbersCollection([1, 5, 8, 0, -3, 12]);
+const sorter = new Sorter(numberCollection);
+sorter.sort();
+console.log(sorter.collection.data);
